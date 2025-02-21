c16dd40fdf1ef1c3b5d3ae175c976d47783c7978
Jan Carlos Anaya Jiménez <janaya@unimagdalena.edu.co>
2025-02-21 16:03:32 -0500

    build(app): config prod environment

---
+++ src/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    detallesFacturaApiUrl: 'https://adminfacturas-cd3933b1469c.herokuapp.com/api/detallesfactura',
+    facturasApiUrl: 'https://adminfacturas-cd3933b1469c.herokuapp.com/api/facturas',
+};
